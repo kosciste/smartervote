257e9c912933d4d6fd05410f0096bdf8a7cfb50c
teodoste <teodoste@students.zhaw.ch>
2021-03-25 04:40:08 +0100

    #13 - creating base entity

---
+++ smartervote-persistency/src/main/java/ch/zhaw/smartervote/persistency/entities/BaseEntity.java
@@ -0,0 +1,49 @@
+package ch.zhaw.smartervote.persistency.entities;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+public class BaseEntity {
+
+    @Id
+    @Column(name="id")
+    private UUID id;
+
+    @Column(name="creation_time")
+    private Date creationTime;
+
+    @Column(name="change_time")
+    private Date changeTime;
+
+    @PrePersist
+    public void prePersist() {
+        id = UUID.randomUUID();
+        creationTime = changeTime = new Date()
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public Date getChangeTime() {
+        return changeTime;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setCreationTime(Date creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public void setChangeTime(Date changeTime) {
+        this.changeTime = changeTime;
+    }
+}
