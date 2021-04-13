3d719bd2aedbd629e094295710eaa192b9628239
krebsrap <krebsrap@students.zhaw.ch>
2021-04-13 20:09:26 +0200

    #41 - add subject wight enum and javadoc for various classes

--- smartervote-domain/src/main/java/ch/zhaw/smartervote/domain/algorithm/ElectionProposalAlgorithm.java
+++ smartervote-domain/src/main/java/ch/zhaw/smartervote/domain/algorithm/ElectionProposalAlgorithm.java
@@ -4,6 +4,9 @@ import ch.zhaw.smartervote.contract.transferobject.QuestionTO;
 import ch.zhaw.smartervote.contract.transferobject.SubjectTO;
 import ch.zhaw.smartervote.persistency.entities.ProposalResult;
 import ch.zhaw.smartervote.persistency.entities.ProposalResultScore;
+import ch.zhaw.smartervote.persistency.repositories.iface.ProposalResultRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
 import java.util.Map;
@@ -15,7 +18,18 @@ import java.util.UUID;
  *
  * @author Raphael Krebs
  */
+@Component("electionProposalAlgorithm")
 public class ElectionProposalAlgorithm {
+
+    /**
+     * The repository for the proposal result.
+     */
+    private final ProposalResultRepository proposalResultRepository;
+
+    @Autowired
+    public ElectionProposalAlgorithm(ProposalResultRepository proposalResultRepository) {
+        this.proposalResultRepository = proposalResultRepository;
+    }
 
     /**
      * Calculates the election based on the answered questions of the user. Not implemented yet, this method currently
@@ -25,15 +39,22 @@ public class ElectionProposalAlgorithm {
      * @param questions the answered questions.
      * @return the UUID of the result.
      */
-    public static UUID calculate(UUID electionId, Map<SubjectTO, Set<QuestionTO>> questions) {
+    public UUID calculate(UUID electionId, Map<SubjectTO, Set<QuestionTO>> questions) {
         ProposalResult proposalResult = new ProposalResult();
-        proposalResult.prePersist();
         Set<ProposalResultScore> proposalResultScores = calculateResults(electionId, questions);
         proposalResult.setProposalResultScores(proposalResultScores);
+        proposalResultRepository.insert(proposalResult);
         return proposalResult.getId();
     }
 
-    private static Set<ProposalResultScore> calculateResults(
+    /**
+     * Calculates the results to be filled into tha database.
+     *
+     * @param electionId the id of the election.
+     * @param questions the answered questions.
+     * @return the proposal result scores for each politician.
+     */
+    private Set<ProposalResultScore> calculateResults(
             UUID electionId, Map<SubjectTO, Set<QuestionTO>> questions) {
         Set<ProposalResultScore> score = new HashSet<>();
         return score;
