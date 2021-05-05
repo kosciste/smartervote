8ecf35e49063b3acc481d478126648af62f9100a
rudinleo <rudinleo@students.zhaw.ch>
2021-05-06 00:22:54 +0200

    #104 - rewrite tests to that they work with the whole spring JPA context

---
+++ smartervote-domain/src/test/java/ch/zhaw/smartervote/domain/AbstractIntegrationTest.java
@@ -0,0 +1,40 @@
+package ch.zhaw.smartervote.domain;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public abstract class AbstractIntegrationTest {
+
+    /**
+     * Use port one number higher than the standard postgres port for the test container database.
+     */
+    private static final int PORT = 5433;
+
+    private static final String POSTGRES_PASSWORD = "123456";
+
+    private static GenericContainer<?> CONTAINER;
+
+    @BeforeAll
+    static void beforeAll() {
+        String createDatabase = AbstractIntegrationTest.class.getResource("/sql/create-database.sql").getPath();
+        String createTables = AbstractIntegrationTest.class.getResource("/sql/create-tables.sql").getPath();
+        String createData = AbstractIntegrationTest.class.getResource("/sql/create-data.sql").getPath();
+
+        CONTAINER = new GenericContainer<>(DockerImageName.parse("draka/smartervote-db:1.0"))
+                .withFileSystemBind(createDatabase, "/create-database.sql")
+                .withFileSystemBind(createTables, "/create-tables.sql")
+                .withFileSystemBind(createData, "/create-data.sql")
+                .withEnv("POSTGRES_PASSWORD", POSTGRES_PASSWORD)
+                .withExposedPorts(PORT);
+
+        CONTAINER.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        CONTAINER.stop();
+    }
+
+}
---
+++ smartervote-domain/src/test/java/ch/zhaw/smartervote/domain/TestApplication.java
@@ -0,0 +1,8 @@
+package ch.zhaw.smartervote.domain;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("ch.zhaw.smartervote")
+public class TestApplication {}
